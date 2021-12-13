53ca7eb1ff50cf7eb5bb6ca4eadfe55ea02178f6
supertiger1234 <saawalkhn@googlemail.com>
2021-12-13 11:42:46 +0000

    fix capital letter noob

--- src/docs/v1/routes/presences/updatePresence.ts
+++ src/docs/v1/routes/presences/updatePresence.ts
@@ -3,7 +3,7 @@ export default {
   name: "Update Presence",
   path: "/update",
   method: "PATCH",
-  description: "Update user presence. 0 = offline, 1 = online, 2 = Away, 3 = Busy",
+  description: "Update user presence. 0 = offline, 1 = online, 2 = away, 3 = busy",
   requestBody: {
     presence: {type: 'Number', example: "1"},
   },
